d626327ae47c4cd6c27c7f92ec31098e4f1345b2
RuiCUI <ailsa1990fr@hotmail.com>
2018-05-04 16:31:57 +0800

    Rotate Array
    思维有限啊~学到了几个不错的想法~~

--- src/solution/array/RotateArray.java
+++ src/solution/array/RotateArray.java
@@ -2,7 +2,7 @@ package solution.array;
 
 /**
  * @author By RuiCUI
- * 2018-04-28
+ * 2018-05-04
  * Easy
  * Question 189:Rotate Array.
  * Given an array, rotate the array to the right by k steps, where k is non-negative.
@@ -25,6 +25,14 @@ package solution.array;
  */
 public class RotateArray {
 
+	/**
+	 * 我自己写的方法 不太优
+	 * 时间复杂度：O(n*k) n的平方
+	 * 空间复杂度：O(1)
+	 * @param nums
+	 * @param k
+	 * @return
+	 */
 	public static void rotate1(int[] nums, int k) {
 		int len = nums.length;
         for(int i=0;i<k;i++){
@@ -36,24 +44,120 @@ public class RotateArray {
         }
     }
 	
+	/**
+	 * 我自己写的方法 不对  没有考虑到k大于n的情况
+	 * 时间复杂度：O(n*n) 
+	 * 空间复杂度：O(n)
+	 * @param nums
+	 * @param k
+	 * @return
+	 */
 	public static void rotate2(int[] nums, int k) {
 		int len = nums.length;
 		int[] n = new int[len];
 		for(int i=0;i<len;i++){
 			n[i] = nums[i];
 		}
-        for(int i=0;i<len-k;i++){
-        	nums[i] = n[len-k-1+i];
-        }
-        int j = 0;
-        for(int i=len-k;i<len;i++){
-        	nums[i] = n[j];
-        	j++;
+		if(k>len){
+			
+		}
+		if(len>1&&k!=0&&k!=len){
+	        for(int i=0;i<len-k-1;i++){
+	        	nums[i] = n[len-k+i];
+	        }
+	        int j = 0;
+	        for(int i=len-k-1;i<len;i++){
+	        	nums[i] = n[j];
+	        	j++;
+	        }
+		}
+    }
+	
+	/**
+	 * 答案1--循环 [Time Limit Exceeded]
+	 * 时间复杂度：O(n*k) 
+	 * 空间复杂度：O(1)
+	 * @param nums
+	 * @param k
+	 * @return
+	 */
+	public void rotate3(int[] nums, int k) {
+        int temp, previous;
+        for (int i = 0; i < k; i++) {
+            previous = nums[nums.length - 1];
+            for (int j = 0; j < nums.length; j++) {
+                temp = nums[j];
+                nums[j] = previous;
+                previous = temp;
+            }
         }
     }
 	
-	public static void rotate3(int[] nums, int k) {
-        
+	/**
+	 * 答案2--Using Extra Array
+	 * 时间复杂度：O(n) 
+	 * 空间复杂度：O(n)
+	 * @param nums
+	 * @param k
+	 * @return
+	 */
+	public void rotate4(int[] nums, int k) {
+        int[] a = new int[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            a[(i + k) % nums.length] = nums[i];
+        }
+        for (int i = 0; i < nums.length; i++) {
+            nums[i] = a[i];
+        }
+    }
+	
+	/**
+	 * 答案3--Using Cyclic Replacements
+	 * 时间复杂度：O(n) 
+	 * 空间复杂度：O(1)
+	 * @param nums
+	 * @param k
+	 * @return
+	 */
+	public void rotate5(int[] nums, int k) {
+        k = k % nums.length;
+        int count = 0;
+        for (int start = 0; count < nums.length; start++) {
+            int current = start;
+            int prev = nums[start];
+            do {
+                int next = (current + k) % nums.length;
+                int temp = nums[next];
+                nums[next] = prev;
+                prev = temp;
+                current = next;
+                count++;
+            } while (start != current);
+        }
+    }
+	
+	/**
+	 * 答案4--Using Reverse
+	 * 时间复杂度：O(n) 
+	 * 空间复杂度：O(1)
+	 * @param nums
+	 * @param k
+	 * @return
+	 */
+	public void rotate6(int[] nums, int k) {
+        k %= nums.length;
+        reverse(nums, 0, nums.length - 1);
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, nums.length - 1);
+    }
+    public void reverse(int[] nums, int start, int end) {
+        while (start < end) {
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+            start++;
+            end--;
+        }
     }
 	
 	public static void main(String[] args) {
