96d09485ffa341a7d15ebc60c6f3359c7924c954
cuirui <ailsa1990fr@hotmail.com>
2018-11-08 13:33:20 +0800

    Shortest Unsorted Continuous Subarray
    这个题看着不难，其实不简单，需要考虑的点很多，不过也有巧妙的解法~

---
+++ src/solution/array/ShortestUnsortedContinuousSubarray.java
@@ -0,0 +1,187 @@
+package solution.array;
+
+import java.util.Arrays;
+import java.util.Stack;
+
+/**
+ * @author By RuiCUI
+ * 2018-11-08
+ * Easy
+ * Question 581:Shortest Unsorted Continuous Subarray.
+ * Given an integer array, you need to find one continuous subarray that if you only sort this subarray in ascending order, 
+ * then the whole array will be sorted in ascending order, too.
+ * You need to find the shortest such subarray and output its length.
+ * Example 1:
+ * Input: [2, 6, 4, 8, 10, 9, 15]
+ * Output: 5
+ * Explanation: You need to sort [6, 4, 8, 10, 9] in ascending order to make the whole array sorted in ascending order.
+ * Note:
+ * 1.Then length of the input array is in range [1, 10,000].
+ * 2.The input array may contain duplicates, so ascending order here means <=.
+ */
+public class ShortestUnsortedContinuousSubarray {
+
+	/**
+	 * 我自己写的方法
+	 * 时间复杂度：O(nlgn)
+	 * 空间复杂度：O(n)
+	 * @param nums
+	 * @return
+	 */
+	public static int findUnsortedSubarray(int[] nums) {
+		int[] snums = nums.clone();
+        Arrays.sort(snums);
+        int start = snums.length, end = 0;
+        for (int i = 0; i < snums.length; i++) {
+            if (snums[i] != nums[i]) {
+                start = Math.min(start, i);
+                end = Math.max(end, i);
+            }
+        }
+        return (end - start >= 0 ? end - start + 1 : 0);
+    }
+	
+	/**
+	 * 答案1--Brute Force[Time Limit Exceeded]
+	 * 时间复杂度：O(n^3) n的立方 
+	 * 空间复杂度：O(1)
+	 * @param prices
+	 * @return
+	 */
+	public int findUnsortedSubarray1(int[] nums) {
+        int res = nums.length;
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i; j <= nums.length; j++) {
+                int min = Integer.MAX_VALUE, max = Integer.MIN_VALUE, prev = Integer.MIN_VALUE;
+                for (int k = i; k < j; k++) {
+                    min = Math.min(min, nums[k]);
+                    max = Math.max(max, nums[k]);
+                }
+                if ((i > 0 && nums[i - 1] > min) || (j < nums.length && nums[j] < max))
+                    continue;
+                int k = 0;
+                while (k < i && prev <= nums[k]) {
+                    prev = nums[k];
+                    k++;
+                }
+                if (k != i)
+                    continue;
+                k = j;
+                while (k < nums.length && prev <= nums[k]) {
+                    prev = nums[k];
+                    k++;
+                }
+                if (k == nums.length) {
+                    res = Math.min(res, j - i);
+
+                }
+            }
+        }
+        return res;
+    }
+	
+	/**
+	 * 答案2--Better Brute Force[Time Limit Exceeded]
+	 * 时间复杂度：O(n^2)
+	 * 空间复杂度：O(1)
+	 * @param nums
+	 * @return
+	 */
+	public int findUnsortedSubarray2(int[] nums) {
+        int l = nums.length, r = 0;
+        for (int i = 0; i < nums.length - 1; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[j] < nums[i]) {
+                    r = Math.max(r, j);
+                    l = Math.min(l, i);
+                }
+            }
+        }
+        return r - l < 0 ? 0 : r - l + 1;
+    }
+    
+    /**
+	 * 答案3--Using Sorting[Accepted]
+	 * 时间复杂度：O(nlgn)
+	 * 空间复杂度：O(n)
+	 * @param nums
+	 * @return
+	 */
+	public int findUnsortedSubarray3(int[] nums) {
+        int[] snums = nums.clone();
+        Arrays.sort(snums);
+        int start = snums.length, end = 0;
+        for (int i = 0; i < snums.length; i++) {
+            if (snums[i] != nums[i]) {
+                start = Math.min(start, i);
+                end = Math.max(end, i);
+            }
+        }
+        return (end - start >= 0 ? end - start + 1 : 0);
+    }
+    
+    /**
+	 * 答案4--Using Stack[Accepted]
+	 * 时间复杂度：O(n)
+	 * 空间复杂度：O(n)
+	 * @param nums
+	 * @return
+	 */
+	public int findUnsortedSubarray4(int[] nums) {
+        Stack < Integer > stack = new Stack < Integer > ();
+        int l = nums.length, r = 0;
+        for (int i = 0; i < nums.length; i++) {
+            while (!stack.isEmpty() && nums[stack.peek()] > nums[i])
+                l = Math.min(l, stack.pop());
+            stack.push(i);
+        }
+        stack.clear();
+        for (int i = nums.length - 1; i >= 0; i--) {
+            while (!stack.isEmpty() && nums[stack.peek()] < nums[i])
+                r = Math.max(r, stack.pop());
+            stack.push(i);
+        }
+        return r - l > 0 ? r - l + 1 : 0;
+    }
+	
+    /**
+	 * 答案5--Without Using Extra Space[Accepted]
+	 * 时间复杂度：O(n)
+	 * 空间复杂度：O(1)
+	 * @param nums
+	 * @return
+	 */
+	public int findUnsortedSubarray5(int[] nums) {
+        int min = Integer.MAX_VALUE, max = Integer.MIN_VALUE;
+        boolean flag = false;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] < nums[i - 1])
+                flag = true;
+            if (flag)
+                min = Math.min(min, nums[i]);
+        }
+        flag = false;
+        for (int i = nums.length - 2; i >= 0; i--) {
+            if (nums[i] > nums[i + 1])
+                flag = true;
+            if (flag)
+                max = Math.max(max, nums[i]);
+        }
+        int l, r;
+        for (l = 0; l < nums.length; l++) {
+            if (min < nums[l])
+                break;
+        }
+        for (r = nums.length - 1; r >= 0; r--) {
+            if (max > nums[r])
+                break;
+        }
+        return r - l < 0 ? 0 : r - l + 1;
+    }
+    
+	public static void main(String[] args) {
+		int[] nums = {1,2,3,4};
+		System.out.println(findUnsortedSubarray(nums));
+	}
+
+}
