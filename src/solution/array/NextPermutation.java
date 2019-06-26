40cdd77824362878abc39b96a349997b5c88e5cc
cuirui <ailsa1990fr@hotmail.com>
2019-06-26 16:53:41 +0800

    Next permutation
    这题不简单，题目没有理解好~

---
+++ src/solution/array/NextPermutation.java
@@ -0,0 +1,97 @@
+package solution.array;
+
+/**
+ * @author By RuiCUI
+ * 2019-06-26
+ * Medium
+ * Question 31:Next Permutation.
+ * Implement next permutation, which rearranges numbers into the lexicographically next greater permutation of numbers.
+ * If such arrangement is not possible, it must rearrange it as the lowest possible order (ie, sorted in ascending order).
+ * The replacement must be in-place and use only constant extra memory.
+ * Here are some examples. Inputs are in the left-hand column and its corresponding outputs are in the right-hand column.
+ * 1,2,3 → 1,3,2
+ * 3,2,1 → 1,2,3
+ * 1,1,5 → 1,5,1.
+ */
+public class NextPermutation {
+
+	/**
+	 * 我自己写的方法
+	 * 时间复杂度：O(n)
+	 * 空间复杂度：O(1)
+	 * @param nums
+	 * @return
+	 */
+	public static void nextPermutation(int[] nums) {
+		int i = nums.length - 2;
+        while (i >= 0 && nums[i + 1] <= nums[i]) {
+            i--;
+        }
+        if (i >= 0) {
+            int j = nums.length - 1;
+            while (j >= 0 && nums[j] <= nums[i]) {
+                j--;
+            }
+            swap(nums, i, j);
+        }
+        reverse(nums, i + 1);
+    }
+
+    private static void reverse(int[] nums, int start) {
+        int i = start, j = nums.length - 1;
+        while (i < j) {
+            swap(nums, i, j);
+            i++;
+            j--;
+        }
+    }
+
+    private static void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+    
+    /**
+	 * 答案--Single Pass Approach
+	 * 时间复杂度：O(n) 
+	 * 空间复杂度：O(1)
+	 * @param nums
+	 * @return
+	 */
+	public void nextPermutation1(int[] nums) {
+        int i = nums.length - 2;
+        while (i >= 0 && nums[i + 1] <= nums[i]) {
+            i--;
+        }
+        if (i >= 0) {
+            int j = nums.length - 1;
+            while (j >= 0 && nums[j] <= nums[i]) {
+                j--;
+            }
+            swap1(nums, i, j);
+        }
+        reverse1(nums, i + 1);
+    }
+
+    private void reverse1(int[] nums, int start) {
+        int i = start, j = nums.length - 1;
+        while (i < j) {
+            swap(nums, i, j);
+            i++;
+            j--;
+        }
+    }
+
+    private void swap1(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+    
+	public static void main(String[] args) {
+		int[] nums = {1,2,3};
+		nextPermutation(nums);
+	}
+
+}
