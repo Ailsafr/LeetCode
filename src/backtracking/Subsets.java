540252db7d73cd95e3388a5b5fac2146d9b2ede0
cuirui <ailsa1990fr@hotmail.com>
2019-08-02 10:56:42 +0800

    Subsets
    这题做的还行~

---
+++ src/backtracking/Subsets.java
@@ -0,0 +1,105 @@
+package backtracking;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+/**
+ * @author By RuiCUI
+ * 2019-08-02
+ * Medium
+ * Question 78:Subsets.
+ * Given a set of distinct integers, nums, return all possible subsets (the power set).
+ * Note: The solution set must not contain duplicate subsets.
+ * Example:
+ * Input: nums = [1,2,3]
+ * Output:
+	[
+	  [3],
+	  [1],
+	  [2],
+	  [1,2,3],
+	  [1,3],
+	  [2,3],
+	  [1,2],
+	  []
+	]
+ */
+public class Subsets {
+
+	/**
+	 * 我自己写的方法
+	 * 时间复杂度：O(n)
+	 * 空间复杂度：O(n)
+	 * @param nums
+	 * @return
+	 */
+	public static List<List<Integer>> subsets(int[] nums) {
+		Set<List<Integer>> set = new HashSet<List<Integer>>();
+		List<Integer> list = Arrays.stream(nums).boxed().collect(Collectors.toList());
+		helper(set, list);
+        return new ArrayList<>(set);
+    }
+	private static void helper(Set<List<Integer>> set, List<Integer> list){
+		if(!set.contains(list)){
+			set.add(list);
+		}
+		for(int i=0;i<list.size();i++){
+			List<Integer> l = new ArrayList<Integer>(list);
+			l.remove(i);
+			helper(set, l);
+		} 
+	}
+	
+	/**
+	 * 官网没有solution,这是其他人的答案
+	 * 时间复杂度：O(n)
+	 * 空间复杂度：O(n)
+	 * @param nums
+	 * @return
+	 */
+	public List<List<Integer>> subsets1(int[] nums) {
+	    List<List<Integer>> list = new ArrayList<>();
+	    Arrays.sort(nums);
+	    backtrack(list, new ArrayList<>(), nums, 0);
+	    return list;
+	}
+	private void backtrack(List<List<Integer>> list , List<Integer> tempList, int [] nums, int start){
+	    list.add(new ArrayList<>(tempList));
+	    for(int i = start; i < nums.length; i++){
+	        tempList.add(nums[i]);
+	        backtrack(list, tempList, nums, i + 1);
+	        tempList.remove(tempList.size() - 1);
+	    }
+	}
+	
+	/**
+	 * 官网没有solution,这是其他人的答案
+	 * 时间复杂度：O(n)
+	 * 空间复杂度：O(n)
+	 * @param nums
+	 * @return
+	 */
+	public List<List<Integer>> subsets2(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        result.add(new ArrayList<>());
+        for(int n : nums){
+            int size = result.size();
+            for(int i=0; i<size; i++){
+                List<Integer> subset = new ArrayList<>(result.get(i));
+                subset.add(n);
+                result.add(subset);
+            }
+        }
+        return result;
+    }
+	
+	public static void main(String[] args) {
+		int[] nums = {1,2,3};
+		System.out.println(subsets(nums));
+	}
+
+}
