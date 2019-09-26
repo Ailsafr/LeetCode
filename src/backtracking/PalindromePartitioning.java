f7a7808b30e98a94eeeb7148525149734917010c
cuirui <ailsa1990fr@hotmail.com>
2019-09-26 13:34:12 +0800

    Palindrome Partitioning
    这题很不错~

---
+++ src/backtracking/PalindromePartitioning.java
@@ -0,0 +1,166 @@
+package backtracking;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author By RuiCUI
+ * 2019-09-26
+ * Medium
+ * Question 131:Palindrome Partitioning.
+ * Given a string s, partition s such that every substring of the partition is a palindrome.
+ * Return all possible palindrome partitioning of s.
+ * Example:
+ * Input: "aab"
+ * Output:
+	[
+	  ["aa","b"],
+	  ["a","a","b"]
+	]
+ */
+public class PalindromePartitioning {
+
+	/**
+	 * 我自己写的方法
+	 * 时间复杂度：O(n)
+	 * 空间复杂度：O(n)
+	 * @param s
+	 * @return
+	 */
+	public static List<List<String>> partition(String s) {
+		int len = s.length();
+		List<List<String>>[] result = new List[len + 1];
+		result[0] = new ArrayList<List<String>>();
+		result[0].add(new ArrayList<String>());
+
+		boolean[][] pair = new boolean[len][len];
+		for (int i = 0; i < s.length(); i++) {
+			result[i + 1] = new ArrayList<List<String>>();
+			for (int left = 0; left <= i; left++) {
+				if (s.charAt(left) == s.charAt(i) && (i-left <= 1 || pair[left + 1][i - 1])) {
+					pair[left][i] = true;
+					String str = s.substring(left, i + 1);
+					for (List<String> r : result[left]) {
+						List<String> ri = new ArrayList<String>(r);
+						ri.add(str);
+						result[i + 1].add(ri);
+					}
+				}
+			}
+		}
+		return result[len];
+    }
+
+	
+	/**
+	 * 官网没有solution,这是其他人的答案
+	 * 时间复杂度：O(n)
+	 * 空间复杂度：O(n)
+	 * @param s
+	 * @return
+	 */
+	List<List<String>> resultLst;
+    ArrayList<String> currLst;
+    public List<List<String>> partition1(String s) {
+        resultLst = new ArrayList<List<String>>();
+        currLst = new ArrayList<String>();
+        backTrack(s,0);
+        return resultLst;
+    }
+    public void backTrack(String s, int l){
+        if(currLst.size()>0 //the initial str could be palindrome
+            && l>=s.length()){
+                List<String> r = (ArrayList<String>) currLst.clone();
+                resultLst.add(r);
+        }
+        for(int i=l;i<s.length();i++){
+            if(isPalindrome(s,l,i)){
+                if(l==i)
+                    currLst.add(Character.toString(s.charAt(i)));
+                else
+                    currLst.add(s.substring(l,i+1));
+                backTrack(s,i+1);
+                currLst.remove(currLst.size()-1);
+            }
+        }
+    }
+    public boolean isPalindrome(String str, int l, int r){
+        if(l==r) return true;
+        while(l<r){
+            if(str.charAt(l)!=str.charAt(r)) return false;
+            l++;r--;
+        }
+        return true;
+    }
+	
+	/**
+	 * 官网没有solution,这是其他人的答案
+	 * 时间复杂度：O(n)
+	 * 空间复杂度：O(n)
+	 * @param s
+	 * @return
+	 */
+    public List<List<String>> partition2(String s) {
+        List<List<String>> res = new ArrayList<>();
+        boolean[][] dp = new boolean[s.length()][s.length()];
+        for(int i = 0; i < s.length(); i++) {
+            for(int j = 0; j <= i; j++) {
+                if(s.charAt(i) == s.charAt(j) && (i - j <= 2 || dp[j+1][i-1])) {
+                    dp[j][i] = true;
+                }
+            }
+        }
+        helper(res, new ArrayList<>(), dp, s, 0);
+        return res;
+    }
+    private void helper(List<List<String>> res, List<String> path, boolean[][] dp, String s, int pos) {
+        if(pos == s.length()) {
+            res.add(new ArrayList<>(path));
+            return;
+        }
+        for(int i = pos; i < s.length(); i++) {
+            if(dp[pos][i]) {
+                path.add(s.substring(pos,i+1));
+                helper(res, path, dp, s, i+1);
+                path.remove(path.size()-1);
+            }
+        }
+    }
+	
+	/**
+	 * 官网没有solution,这是其他人的答案
+	 * 时间复杂度：O(n)
+	 * 空间复杂度：O(n)
+	 * @param s
+	 * @return
+	 */
+    public static List<List<String>> partition3(String s) {
+		int len = s.length();
+		List<List<String>>[] result = new List[len + 1];
+		result[0] = new ArrayList<List<String>>();
+		result[0].add(new ArrayList<String>());
+
+		boolean[][] pair = new boolean[len][len];
+		for (int i = 0; i < s.length(); i++) {
+			result[i + 1] = new ArrayList<List<String>>();
+			for (int left = 0; left <= i; left++) {
+				if (s.charAt(left) == s.charAt(i) && (i-left <= 1 || pair[left + 1][i - 1])) {
+					pair[left][i] = true;
+					String str = s.substring(left, i + 1);
+					for (List<String> r : result[left]) {
+						List<String> ri = new ArrayList<String>(r);
+						ri.add(str);
+						result[i + 1].add(ri);
+					}
+				}
+			}
+		}
+		return result[len];
+	}
+	
+	public static void main(String[] args) {
+		String s = "aab";
+		System.out.println(partition(s));
+	}
+
+}
